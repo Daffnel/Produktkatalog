1b0db752fd1ab0bc26539fac1fa7e54a4fa85a42
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-10-16 11:19:14 +0200

    Updated Catalog class to handle new sublclasses

--- src/Catalog.java
+++ src/Catalog.java
@@ -4,25 +4,46 @@ public class Catalog {
     private final ArrayList<Product> allProducts;
 
 
-
-
     public Catalog() {
 
-            this.allProducts = new ArrayList<>();
-        }
+        this.allProducts = new ArrayList<>();
+    }
 
-        public void addProduct (Product product){
-            allProducts.add(product);
-            System.out.println(product + " was added to the catalog.");
-        }
+    public void addProduct(Product product) {
+        allProducts.add(product);
+        System.out.println(product + " was added to the catalog.");
+    }
 
-        public void printAllProducts () {
-            for (Product product : allProducts) {
+    public void printAllProducts() {
+        System.out.println("Foods: ");
+        for (Product product : allProducts) {
+            if (product instanceof FoodProduct) {
                 System.out.println(product);
             }
         }
-
-        public ArrayList<Product> getAllProducts () {
-            return allProducts;
+        System.out.println("Electronics: ");
+        for (Product product : allProducts) {
+            if (product instanceof ElectronicProduct) {
+                System.out.println(product);
+            }
         }
     }
+
+    public void printFoodProducts() {
+        System.out.println("Foods:");
+        for (Product product : allProducts) {
+            if (product instanceof FoodProduct) {
+                System.out.println(product);
+            }
+        }
+    }
+
+    public void printElectronicProducts() {
+        System.out.println("Electronics:");
+        for (Product product : allProducts) {
+            if (product instanceof ElectronicProduct) {
+                System.out.println(product);
+            }
+        }
+    }
+}
