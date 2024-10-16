92a8e399942f7eb9cf6e3de01cc09bd153b26569
Magnus Freidenfelt <sabbathmagnus@gmail.com>
2024-10-16 09:48:54 +0200

    Added Catalog class and methods for printing all methods and adding products.

---
+++ src/Catalog.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Catalog {
+    private final ArrayList<Product> allProducts;
+
+    public Catalog(ArrayList<Product> allProducts) {
+        this.allProducts = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        allProducts.add(product);
+        System.out.println(product + " was added to the catalog.");
+    }
+
+    public void printAllProducts() {
+        for (Product product : allProducts) {
+            System.out.println(product);
+        }
+    }
+
+    public ArrayList<Product> getAllProducts() {
+        return allProducts;
+    }
+}
