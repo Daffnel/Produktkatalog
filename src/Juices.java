1fbd36000900d770cbc593b80143c409caf98255
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2024-10-16 10:07:30 +0200

    Juicer

---
+++ src/Juices.java
@@ -0,0 +1,14 @@
+public class Juices extends Product{
+
+    public Juices(String name, Double price) {
+        super(name, price);
+    }
+
+    public void OrangeJuice{
+        System.out.println("A smidge sour");
+    }
+
+    public void AppleJuice{
+        System.out.println("A tasty treat for your tastebuds!");
+    }
+}
