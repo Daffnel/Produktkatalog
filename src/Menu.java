d40e204f731d9f315cd060608d92ab915a64d5d9
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2024-10-16 10:16:23 +0200

    commit 2

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Menu {
+
+    public static void printMenu() {
+        System.out.println("** MENY **");
+        System.out.println("1. Lägg till produkt");
+        System.out.println("2  Skriv ut listan");
+    }
+
+
 }
--- src/Product.java
+++ src/Product.java
@@ -3,4 +3,7 @@ public class Product {
     private String name;
     private double price;
 
+
+
+
 }
