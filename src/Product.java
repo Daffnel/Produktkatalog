8ab577532625be2e5826890c7e4fbb6270ab6510
Simon-E-Mapp <3ggapp24.baugsi@folkuniversitetet.nu>
2024-10-16 11:34:13 +0200

    Snyggat till utskrift i Product

--- src/Product.java
+++ src/Product.java
@@ -4,13 +4,12 @@ public class Product {
     private double price;
 
     public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
     }
 
     @Override
     public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                '}';
+        return name + " " + price + ":-";
     }
 }
